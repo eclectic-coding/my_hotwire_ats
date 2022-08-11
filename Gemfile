bdae88384e4080d470653ef7805e1c9017f51197
Chuck Smith <chuck.smith@opscompass.com>
2022-08-11 07:04:49 -0400

    Add devise, custom CSS, and style registration and sign in views

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "jbuilder"
 gem "redis", "~> 4.0"
 gem "bootsnap", require: false
 gem "stimulus_reflex", "= 3.5.0.pre9"
+gem 'devise', branch: 'rails_7', github: 'DavidColby/devise'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  get 'dashboard/show'
-  root 'dashboard#show'
+  devise_for :users,
+             path: '',
+             controllers: {
+               registrations: 'users/registrations',
+               sessions: 'users/sessions'
+             },
+             path_names: {
+               sign_in: 'login',
+               password: 'forgot',
+               confirmation: 'confirm',
+               sign_up: 'sign_up',
+               sign_out: 'signout'
+             }
+  # Snip
 
+  authenticated :user do
+    root to: 'dashboard#show', as: :user_root
+  end
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
