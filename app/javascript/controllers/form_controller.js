82e6d605f1c8fb6b663fb11f41701dbcbbbf83cc
Chuck Smith <chuck.smith@opscompass.com>
2022-08-15 08:26:22 -0400

    Add job filtering

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,15 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "stimulus"
 import debounce from "lodash/debounce"
 
-// Connects to data-controller="form"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = [ "form" ]
 
   connect() {
     this.submit = debounce(this.submit.bind(this), 200)
   }
 
-  submit(){
+  submit() {
     this.formTarget.requestSubmit()
   }
 }
