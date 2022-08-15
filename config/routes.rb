23c54d97c942af35836ee723b0cc65fda911a49e
Chuck Smith <chuck.smith@opscompass.com>
2022-08-15 09:18:15 -0400

    Build out applicants show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :applicants do
     patch :change_stage, on: :member
+    get :resume, action: :show, controller: 'resumes'
   end
+
   resources :jobs
   devise_for :users,
              path: '',
